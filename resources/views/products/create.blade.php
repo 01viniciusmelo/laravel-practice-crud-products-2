2f6bd103fb85b4e1b38ff4483a13aa0234b86c73
Marianna Pessolano <marianna.pessolano@gmail.com>
2018-06-06 19:57:54 -0400

    [#4] CREATE - Crear una vista para agregar productos a la base de datos
    Crear el directorio products dentro de resources/views y adentro la vista create.blade.php

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 5.6.24 CRUD Tutorial With Example From Scratch </title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div class="container">
+        <h2>Create a Product</h2><br>
+        <form action="" method="post">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="form-group col-md-4">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" name="name">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="form-group col-md-4">
+                    <label for="price">Price:</label>
+                    <input type="text" class="form-control" name="price">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="form-group col-md-4">
+                    <button type="submit" class="btn btn-success" style="margin-left: 38px">Add Product</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
